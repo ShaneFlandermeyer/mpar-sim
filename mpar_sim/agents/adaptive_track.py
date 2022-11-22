045dca0852c1a1c267d5649ef5f471f2cea57ab5
ShaneFlandermeyer <shaneflandermeyer@gmail.com>
2022-11-22 11:27:58 -0600

    WIP: Added a check to keep tracks from being updated multiple times in the same timestep

--- mpar_sim/agents/adaptive_track.py
+++ mpar_sim/agents/adaptive_track.py
@@ -178,7 +178,8 @@ class AdaptiveTrackAgent(Agent):
         self.update_queue.append((next_update_time, track))
         
       elif track in self.confirmed_tracks:
-        track.append(hypothesis.prediction)
+        if current_time > track.states[-1].timestamp:
+          track.append(hypothesis.prediction)
 
     # Try to initiate new tracks from detections that were not associated with any existing tracks
     self.confirmed_tracks -= self.deleter.delete_tracks(self.confirmed_tracks)
