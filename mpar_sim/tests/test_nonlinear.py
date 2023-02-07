017d5eeeeb99b6c6e509cbcd33c946b1a332d185
Shane Flandermeyer <shaneflandermeyer@gmail.com>
2023-02-07 12:22:47 -0600

    Debugged jacobian computation for radar measurement model

--- mpar_sim/tests/test_nonlinear.py
+++ mpar_sim/tests/test_nonlinear.py
@@ -16,17 +16,23 @@ class TestCartesianToAzElBearingRange:
     az = np.rad2deg(np.arctan2(state[2], state[0]))
     rr = np.dot(state[self.model.position_mapping],
                 state[self.model.velocity_mapping]) / r
-    expected = np.array([el, az, r, rr])[:, np.newaxis]
+    expected = np.array([az, el, r, rr])[:, np.newaxis]
 
     actual = self.model.function(state, noise=False)
 
-    assert np.all(expected == actual)
+    assert np.allclose(expected, actual)
 
-  # def test_jacobian(self):
-  #   model = CartesianToRangeAzElRangeRate()
-  #   state = np.ones((6,1))
-  #   J = model.jacobian(state)
-  #   assert False
+  def test_jacobian(self):
+    # TODO: Compare this with the matlab output
+    state = np.array([1, 10, 2, 20, 3, 30])
+    actual = self.model.jacobian(state)
+    expected = np.array(
+        [[-22.9183, 0, 11.4592, 0, 0, 0],
+         [-5.4907, 0, -10.9815, 0, 9.1512, 0],
+            [0.2673, 0, 0.5345, 0, 0.8018, 0],
+            [0, 0.2673, 0, 0.5345, 0, 0.8018]
+         ])
+    assert np.allclose(expected, actual, atol=1e-4)
 
 
 if __name__ == '__main__':
