3884cfa1dad84e452a2a5433e065eeb60f36cf98
ShaneFlandermeyer <shaneflandermeyer@gmail.com>
2023-07-10 13:54:30 -0700

    Added step function to trajectory class

--- mpar_sim/types/trajectory.py
+++ mpar_sim/types/trajectory.py
@@ -7,12 +7,21 @@ from mpar_sim.types.state import State
 
 class Trajectory():
   def __init__(self,
-               states=[],
-               id=None):
+               states = [],
+               transition_model = None,
+               id = None):
     if not isinstance(states, list):
       states = [states]
     self.states = states
     self.id = id if id else str(uuid.uuid1())
+
+  def step(self, 
+           transition_model: callable, 
+           nsteps=1, 
+           **kwargs):
+    for _ in range(nsteps):
+      state = transition_model(self.states[-1].state, **kwargs)
+      self.append(state)
 
   def append(self, state: State, **kwargs):
     if isinstance(state, np.ndarray):
