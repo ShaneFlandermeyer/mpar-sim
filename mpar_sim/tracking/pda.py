c63265f25d6a7d877b573ea1d221bf315cfaa2c8
Shane Flandermeyer <shaneflandermeyer@gmail.com>
2023-07-13 15:38:21 -0500

    Fixed PDA class implementation and reduced python version dependency

--- mpar_sim/tracking/pda.py
+++ mpar_sim/tracking/pda.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 import numpy as np
 from mpar_sim.models.transition import ConstantVelocity
 from mpar_sim.models.measurement.linear import LinearMeasurementModel
@@ -24,113 +24,149 @@ class PDAFilter():
     self.pd = pd
     self.pg = pg
     self.clutter_density = clutter_density
-
-  def predict(self, dt: float):
-    self.filter.predict(dt)
-
-  def gate(self, measurements: List[np.ndarray]) -> List[np.ndarray]:
-    # TODO: Apply an ellipsoidal gate
+    
+  def gate(self, measurements: List[np.ndarray]) -> np.ndarray:
     G = gate_threshold(pg=self.pg,
                        ndim=self.measurement_model.ndim)
-    valid = ellipsoid_gate(measurements=measurements,
-                           predicted_measurement=self.filter.z_pred,
-                           innovation_covar=self.filter.S,
-                           threshold=G)
-    return [m for i, m in enumerate(measurements) if valid[i]]
+    in_gate = ellipsoid_gate(measurements=measurements,
+                             predicted_measurement=self.filter.z_pred,
+                             innovation_covar=self.filter.S,
+                             threshold=G)
+    return [m for m, g in zip(measurements, in_gate) if g]
 
   def update(self,
              measurements: List[np.ndarray],
              dt: float,
-             ):
+             ) -> Tuple[np.ndarray]:
+    # Get the predicted state/covariance/measurement, along with the innovation covariance and Kalman gain.
     self.filter.predict(dt)
-    if len(measurements) == 0:
-      self.filter.x, self.filter.P = self.filter.x_pred, self.filter.P_pred
-      return
-    # This update is just used to get K, S, and z_pred for the PDA update step. These don't depend on the measurement, so it doesn't matter which one we pass in. I'm using m[0] for simplicity
-    self.filter.update(measurement=measurements[0])
+    self.filter.update(measurement=np.empty(self.measurement_model.ndim))
+
+    # Handle gating
     gated_measurements = self.gate(measurements)
-    if len(gated_measurements) == 0:
-      self.filter.x, self.filter.P = self.filter.x_pred, self.filter.P_pred
+    # Compute clutter density.
+    if self.clutter_density:
+      clutter_density = self.clutter_density
     else:
-      self.filter.x, self.filter.P = self._update(
+      m = len(gated_measurements)
+      # For m validated measurements, the clutter density is m / V
+      V_gate = gate_volume(innovation_covar=self.filter.S,
+                           gate_probability=self.pg,
+                           ndim=self.measurement_model.ndim)
+      clutter_density = m / V_gate
+
+    # Compute association probabilities for gated measurements
+    if len(gated_measurements) == 0:
+      betas = np.ones(1,)
+    else:
+      betas = self._association_probs(
           z=gated_measurements,
-          x_pred=self.filter.x_pred,
-          P_pred=self.filter.P_pred,
-          K=self.filter.K,
           z_pred=self.filter.z_pred,
           S=self.filter.S,
           pd=self.pd,
           pg=self.pg,
-          clutter_density=self.clutter_density,
+          clutter_density=clutter_density,
       )
+
+    self.filter.x, self.filter.P = self._update_state(
+        z=gated_measurements,
+        x_pred=self.filter.x_pred,
+        P_pred=self.filter.P_pred,
+        K=self.filter.K,
+        z_pred=self.filter.z_pred,
+        S=self.filter.S,
+        betas=betas,
+    )
     return self.filter.x, self.filter.P
 
   @staticmethod
-  def _update(
-      z: np.array,
-      # Filter parameters
-      x_pred: np.array,
-      P_pred: np.array,
-      K: np.array,
+  def _association_probs(
+      z: List[np.array],
       z_pred: np.array,
       S: np.array,
-      # Parameters
       pd: float,
       pg: float,
-      clutter_density: float = None,
-  ):
+      clutter_density: float,
+  ) -> np.ndarray:
     """
-    Probabilistic Data Association (PDA) filter.
-
-    TODO: probably want to make this (and the kalman filters) into an object to make this easier to manage.
+    Compute the association probabilities for each measurement in the list of gated measurements.
 
     Parameters
     ----------
-    x_pred : np.array
-        _description_
-    P_pred : np.array
-        _description_
-    K : np.array
-        _description_
-    z : np.array
-        _description_
+    z : List[np.array]
+        Gated measurements
     z_pred : np.array
-        _description_
+        Predicted track measurement
     S : np.array
-        _description_
+        Innovation covar
     pd : float
-        _description_
+        Probability of detection
     pg : float
-        _description_
+        Gate probability
+    clutter_density : float
+        Density of the spatial Poisson process that models the clutter
 
     Returns
     -------
-    _type_
-        _description_
+    np.ndarray
+        Length-m+1 array of association probabilities. The first element is the probability of no detection.
     """
-    # TODO: Gate detections
-
     m = len(z)
-    if not clutter_density:
-      # Compute clutter density.
-      # For m validated measurements, the clutter density is m / V
-      V_gate = gate_volume(innovation_covar=S,
-                           gate_probability=pg,
-                           ndim=z_pred.size)
-      clutter_density = m / V_gate
-    # Filter measurements
-
-    # Compute association probabilities
+    betas = np.empty(m+1)
+    # Probability of no detection
+    betas[0] = 1 - pd*pg
+    # Probability of each detection from likelihood ratio
     l = multivariate_normal.pdf(
         z,
         mean=z_pred,
         cov=S,
     )
     l_ratio = l * pd / clutter_density
-    betas = np.empty(m+1)
-    betas[0] = 1 - pd*pg
     betas[1:] = l_ratio
-    betas /= (1 - pd*pg + np.sum(l_ratio))
+
+    # Normalize to sum to 1
+    betas /= np.sum(betas)
+    return betas
+
+  @staticmethod
+  def _update_state(
+      z: List[np.array],
+      # Filter parameters
+      x_pred: np.array,
+      P_pred: np.array,
+      z_pred: np.array,
+      K: np.array,
+      S: np.array,
+      betas: np.ndarray,
+  ) -> Tuple[np.ndarray]:
+    """
+    Compute the posterior state and covariance for the given track as a Gaussian mixture
+
+    Parameters
+    ----------
+    z : List[np.array]
+      List of gated measurements
+    x_pred : np.array
+        Predicted state
+    P_pred : np.array
+        Predicted covar
+    z_pred : np.array
+        Predicted measurement
+    K : np.array
+        Kalman gain matrix
+    S : np.array
+        Innovation covar
+    betas : np.ndarray
+        Association probabilities
+
+    Returns
+    -------
+    Tuple[np.ndarray]
+        Posterior state and covar
+    """
+    # If there are no gated measurements, return the predicted state and covariance
+    if len(z) == 0:
+      return x_pred, P_pred
 
     # State estimation
     # Bar-Shalom2009 - Equations 39-40
@@ -146,6 +182,9 @@ class PDAFilter():
     P_post = betas[0]*P_pred + (1 - betas[0])*Pc + Pt
 
     return x_post, P_post
+  
+  def predict(self, dt: float):
+    self.filter.predict(dt)
 
   @property
   def x(self):
@@ -171,7 +210,7 @@ def test_pda():
   seed = 0
   np.random.seed(seed)
   transition_model = ConstantVelocity(ndim_pos=2,
-                                      noise_diff_coeff=0.0005,
+                                      noise_diff_coeff=0.005,
                                       seed=seed)
   trajectory = Trajectory()
   trajectory.append(state=np.array([0, 1, 0, 1]),
@@ -200,10 +239,9 @@ def test_pda():
       # clutter_density=0.125,
   )
   detections = []
-  # track_pos = np.zeros((len(trajectory), 2))
   track_pos = []
   for i, state in enumerate(trajectory):
-    current_time += dt
+
     current_detections = []
     if np.random.rand() < pd:
       measurement = measurement_model(state.state, noise=True)
@@ -226,12 +264,13 @@ def test_pda():
       current_detections.append(detection)
 
     # Gate detections
-    pda.update(measurements=[d.measurement for d in current_detections], dt=current_time - last_update)
-    # if not np.all(pda.x == pda.filter.x_pred):
-    last_update = current_time
-    track_pos.append(pda.x[np.array([0, 2])])
-
-    detections.extend(current_detections)
+    if len(current_detections) > 0:
+      pda.update(measurements=[
+                 d.measurement for d in current_detections], dt=current_time - last_update)
+      track_pos.append(pda.x[np.array([0, 2])])
+      detections.extend(current_detections)
+      last_update = current_time
+    current_time += dt
 
   states = np.array([state.state for state in trajectory])
   true_detections = np.array(
@@ -242,26 +281,13 @@ def test_pda():
   plt.plot(states[:, 0], states[:, 2], label='Ground Truth')
   plt.plot(true_detections[:, 0], true_detections[:, 1],
            'o', label='True Detections')
-  # plt.plot(false_detections[:, 0], false_detections[:,
-  #          1], 'x', label='False Detections')
+  plt.plot(false_detections[:, 0], false_detections[:,
+           1], 'x', label='False Detections')
   plt.plot(track_pos[:, 0], track_pos[:, 1], '.-', label='Track')
-  plt.xlim([-40, 60])
-  plt.ylim([-10, 60])
+  # plt.xlim([-40, 60])
+  # plt.ylim([-10, 60])
   plt.show()
 
 
 if __name__ == '__main__':
   test_pda()
-#   x_post, P_post = pda_update(
-#       x_pred=np.random.uniform(size=6),
-#       P_pred=np.eye(6),
-#       W=np.random.uniform(size=(6, 6)),
-#       z=[np.random.uniform(size=6) for _ in range(100)],
-#       z_pred=np.random.uniform(size=6),
-#       S=np.eye(6),
-#       pd=0.9,
-#       pg=0.99,
-#   )
-
-#   print(x_post)
-#   print(P_post)
