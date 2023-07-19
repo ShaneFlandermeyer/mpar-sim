41a671fc5f7fc43cdab5cb0801db456279e8d358
Shane Flandermeyer <shaneflandermeyer@gmail.com>
2023-07-19 12:02:20 -0500

    WIP: May have implemented working generalized auction mechanism

--- mpar_sim/tracking/auction.py
+++ mpar_sim/tracking/auction.py
@@ -6,50 +6,74 @@ Algorithm steps:
 3. Find the "best" track for each observation j.
 4. Unassign the observation previously assigned to i_j and assign track i_j to observation j
 5. Set the price of i_j to the level at which the observation j is almost happy.
-6. Return to step 2. 
+6. Return to step 2.
 """
 from typing import List, Optional, Tuple
 import numpy as np
 from collections import deque
 
 
+def is_happy(cost_mat: np.ndarray,
+             prices: np.ndarray,
+             agent_inds: np.ndarray,
+             object_inds: np.ndarray,
+             eps: float):
+  happy = np.zeros(len(agent_inds), dtype=bool)
+  if np.any(object_inds != None):
+    valid_inds = np.where(object_inds != None)[0]
+    agent_inds = agent_inds[valid_inds]
+    object_inds = object_inds[valid_inds].astype(int)
+    happy[agent_inds] = np.max(cost_mat[agent_inds] - prices, axis=1) - \
+      (cost_mat[agent_inds, object_inds] - prices[object_inds]) < eps
+      
+  return happy
+
+
 def auction(
-    assignment_matrix: np.ndarray,
+    a: np.ndarray,
     eps: float,
     maxiter: Optional[int] = None,
 ) -> Tuple[List[Tuple], np.ndarray]:
 
-  nobs, ntrack = assignment_matrix.shape
+  n_object, n_agent = a.shape
+  assert n_object >= n_agent, "Number of agents must be greater than number of objects."
 
   # Initialize all observations as unassociated
-  mask = np.ones(ntrack, dtype=bool)
-  prices = np.zeros(ntrack)
-  unassigned = deque([i for i in range(nobs)])
-  assignments = []
-
+  agents = np.arange(n_agent)
+  assigned_objects = [None for _ in range(n_agent)]
+  happy = np.zeros(n_agent, dtype=bool)
+  prices = np.zeros(n_object)
   iter = 0
   while True:
-    obs_ind = unassigned.popleft()
-    # Find the best track for the current observation
-    track_ind = np.argmax(assignment_matrix[obs_ind] - prices)
-    # Unassign the observation previously assigned to ij (if any) and assign ij to j
-    unassigned.extend([o for o, t in assignments if t == track_ind])
-    assignments = list(filter(lambda x: x[1] != track_ind, assignments))
-    assignments.append((obs_ind, track_ind))
-    if len(unassigned) == 0:
-      break
+    iagent = np.where(~happy)[0][0]
+    
+    # Exchange favored object with the person currently assigned to it
+    iobj = np.argmax(a[iagent] - prices)    
+    if iobj in assigned_objects:
+      # Swap the assigned objects
+      iswap = np.where(assigned_objects == iobj)[0].item()
+      assigned_objects[iswap], assigned_objects[iagent] = \
+          assigned_objects[iagent], assigned_objects[iswap]
+    else:
+      assigned_objects[iagent] = iobj
 
-    # Update the price of the assigned track
-    mask[:] = True
-    mask[track_ind] = False
-    yj = assignment_matrix[obs_ind][track_ind] - \
-        prices[track_ind] - np.max(assignment_matrix[obs_ind][mask])
-    prices[track_ind] += yj + eps
-
+    # Update the price of the assigned objects
+    mask = np.arange(n_object) != iobj
+    yj = a[iagent, iobj] - prices[iobj] - \
+        np.max(a[iagent][mask] - prices[mask])
+    prices[iobj] += yj + eps
     iter += 1
-    if maxiter is not None and iter >= maxiter:
+    
+    happy = is_happy(cost_mat=a,
+                     prices=prices,
+                     agent_inds=agents,
+                     object_inds=np.array(assigned_objects),
+                     eps=eps)
+    if np.all(happy):
       break
 
-  # Sort assignments by observation index
-  assignments = sorted(assignments, key=lambda x: x[0])
-  return assignments, prices
+
+  # Sort the assigned array based on the first column
+  # assigned = assigned[np.argsort(assigned[:, 0])]
+
+  return [(a, o) for a,o in zip(agents, assigned_objects)], prices
