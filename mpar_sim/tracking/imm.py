17db2d2742cb7915a777c469e30db10c4cc71c9e
Shane Flandermeyer <shaneflandermeyer@gmail.com>
2023-07-24 11:29:09 -0500

    Implemented working IMM filter

--- mpar_sim/tracking/imm.py
+++ mpar_sim/tracking/imm.py
@@ -5,6 +5,15 @@ from scipy.stats import multivariate_normal
 
 
 class IMMEstimator():
+  """
+  IMM estimator for tracking a single target with multiple models.
+  
+  The notation for all mathematical operations follows [1]
+  
+  Sources
+  -------
+  [1] Bar-Shalom, Y., Li, X-R., and Kirubarajan, T. “Estimation with Application to Tracking and Navigation”. Wiley-Interscience, 2001.
+  """
   def __init__(self,
                filters: List[KalmanFilter],
                init_probs: np.ndarray,
@@ -18,16 +27,15 @@ class IMMEstimator():
               states: List[np.ndarray],
               covars: List[np.ndarray],
               dt: float) -> Tuple[List[np.ndarray], List[np.ndarray]]:
-    # Step 1: Update mixing probabilities
-    mix_probs = self._mixing_probs(self.mode_probs, self.transition_probs)
-
-    # Step 2: Mix initial conditions
-    mixed_states, mixed_covars = self._mixed_init_conds(
-        x=states, P=covars, mu=mix_probs)
+    
+    # Step 2: Mix the priors of each filter based on the mixing probabilities computed from step 1
+    x_mix, P_mix = self._mixed_init_conds(
+        x=states, P=covars, mu_mix=self.mix_probs)
     predicted_states, predicted_covars = [], []
-    for i, filter in enumerate(self.filters):
-      x_pred, P_pred = filter.predict(state=mixed_states[i],
-                                      covar=mixed_covars[i],
+    # Step 3.0: Get predictions from each filter
+    for j, filter in enumerate(self.filters):
+      x_pred, P_pred = filter.predict(state=x_mix[j],
+                                      covar=P_mix[j],
                                       dt=dt)
       predicted_states.append(x_pred)
       predicted_covars.append(P_pred)
@@ -38,55 +46,62 @@ class IMMEstimator():
              measurement: np.ndarray,
              predicted_states: List[np.ndarray],
              predicted_covars: List[np.ndarray]) -> Tuple[np.ndarray]:
-    likelihoods = []
-    for i, f in enumerate(self.filters):
-      _, _, S, _, z_pred = f.update(
+    r = len(self.filters)
+    ndim_state = predicted_states[0].size
+    
+    # Compute the likelihood 
+    likelihoods = np.empty(r)
+    x_posts = np.empty((r, ndim_state))
+    P_posts = np.empty((r, ndim_state, ndim_state))
+    for j in range(r):
+      # Step 3.0: Incorporate the measurement into each filter
+      x, P, S, _, z_pred = self.filters[j].update(
           measurement=measurement,
-          predicted_state=predicted_states[i],
-          predicted_covar=predicted_covars[i],
+          predicted_state=predicted_states[j],
+          predicted_covar=predicted_covars[j],
       )
+      x_posts[j] = x
+      P_posts[j] = P
+      # Step 3.5 Compute likelihoods for each mode
       l = multivariate_normal.pdf(
-        x=measurement,
-        mean=z_pred,
-        cov=S,
+          x=measurement,
+          mean=z_pred,
+          cov=S,
       )
-      likelihoods.append(l)
-    # TODO: Update the mode probability
-    # TODO: Estimate and covariance combination
-    pass
+      likelihoods[j] = l
+      
+
+    # Step 4: Update the mode probabilities
+    cbar = np.dot(self.mode_probs, self.transition_probs)
+    mu = likelihoods * cbar
+    self.mode_probs = mu / np.sum(mu)
+
+    # Step 5: Combine model-conditioned state estimates and covariances
+    x_mix = np.einsum('j, ji->i', self.mode_probs, x_posts)
+    y = x_posts - x_mix
+    P_mix = np.einsum('j, jik->ik', self.mode_probs, 
+                      P_posts + np.einsum('ji, jk->jik', y, y))
+    return x_posts, P_posts, x_mix, P_mix
 
   # Inidividual algorithm steps as functions from Bar-Shalom2001 section 11.6.6
-  def _mixing_probs(self, mu: np.ndarray, M: np.ndarray):
-    cbar = np.dot(mu, M)
-    return np.einsum('ij, i->ij', M, mu) / cbar
+  @property
+  def mix_probs(self):
+    # Step 1: compute mixing probabilities
+    cbar = np.dot(self.mode_probs, self.transition_probs)
+    return np.einsum('ij, i->ij', self.transition_probs, self.mode_probs) / cbar
 
   def _mixed_init_conds(self,
                         x: List[np.ndarray],
                         P: List[np.ndarray],
-                        mu: np.ndarray
+                        mu_mix: np.ndarray
                         ) -> Tuple[List[np.ndarray], List[np.ndarray]]:
     r = len(x)
     x = np.array(x).reshape(r, -1)
-    x_mix = np.einsum('ij, ik->jk', mu, x)
+    x_mix = np.einsum('ij, ik->jk', mu_mix, x)
 
-    # TODO: SPEED THIS UP or numbafy it
-    P = np.array(P)
     P_mix = np.zeros_like(P)
     for j in range(r):
-      for i in range(r):
-        y = x[i] - x_mix[j]
-        P_mix[j] += mu[i, j] * (P[i] + np.outer(y, y))
-
+      y = x - x_mix[j]
+      P_mix[j] = np.einsum('j, jik->ik', mu_mix[:, j],
+                            P + np.einsum('ji, jk->jik', y, y))
     return list(x_mix), list(P_mix)
-
-  def _mode_matched_filter(self):
-    # Step 3
-    pass
-
-  def _update_mode_probabilities(self):
-    # Step 4
-    pass
-
-  def _mix_estimates(self):
-    # Step 5
-    pass
--- tests/test_imm.py
+++ tests/test_imm.py
@@ -2,6 +2,7 @@ import numpy as np
 from filterpy.common import kinematic_kf
 import filterpy.kalman
 from mpar_sim.tracking import IMMEstimator, KalmanFilter
+import pytest
 
 
 class DummyTransition():
@@ -31,43 +32,50 @@ class DummyMeasurement():
   def covar(self):
     return self.filter.R
 
+def test_imm():
+  # Compare the results of my IMM implementation to filterpy
+  # Create two KFs
+  kf1 = kinematic_kf(2, 2)
+  kf2 = kinematic_kf(2, 2)
+  # do some settings of x, R, P etc. here, I'll just use the defaults
+  kf2.Q *= 0   # no prediction error in second filter
 
-kf1 = kinematic_kf(2, 2)
-kf2 = kinematic_kf(2, 2)
-# do some settings of x, R, P etc. here, I'll just use the defaults
-kf2.Q *= 0   # no prediction error in second filter
+  # Create mpar-sim variants
+  my_kf1 = KalmanFilter(transition_model=DummyTransition(kf1),
+                        measurement_model=DummyMeasurement(kf1))
+  my_kf2 = KalmanFilter(transition_model=DummyTransition(kf2),
+                        measurement_model=DummyMeasurement(kf2))
+  my_imm = IMMEstimator(filters=[my_kf1, my_kf2],
+                        init_probs=[0.5, 0.5],
+                        transition_probs=np.array([[0.97, 0.03], [0.03, 0.97]]))
 
-# Create mpar-sim variants
-my_kf1 = KalmanFilter(transition_model=DummyTransition(kf1),
-                      measurement_model=DummyMeasurement(kf1))
-my_kf2 = KalmanFilter(transition_model=DummyTransition(kf2),
-                      measurement_model=DummyMeasurement(kf2))
-my_imm = IMMEstimator(filters=[my_kf1, my_kf2],
-                      init_probs=[0.5, 0.5],
-                      transition_probs=np.array([[0.97, 0.03], [0.03, 0.97]]))
-my_imm._mixing_probs(mu=my_imm.mode_probs, M=my_imm.transition_probs)
+  filters = [kf1, kf2]
+  mu = [0.5, 0.5]  # each filter is equally likely at the start
+  trans = np.array([[0.97, 0.03], [0.03, 0.97]])
+  imm = filterpy.kalman.IMMEstimator(filters, mu, trans)
 
-filters = [kf1, kf2]
-mu = [0.5, 0.5]  # each filter is equally likely at the start
-trans = np.array([[0.97, 0.03], [0.03, 0.97]])
-imm = filterpy.kalman.IMMEstimator(filters, mu, trans)
-
-states = [kf1.x.squeeze(), kf2.x.squeeze()]
-covars = [kf1.P, kf2.P]
-for i in range(100):
+  states = [kf1.x.squeeze(), kf2.x.squeeze()]
+  covars = [kf1.P, kf2.P]
+  for i in range(100):
     # make some noisy data
-  x = i + np.random.randn()*np.sqrt(kf1.R[0, 0])
-  y = i + np.random.randn()*np.sqrt(kf1.R[1, 1])
-  z = np.array([x, y])
+    x = i + np.random.randn()*np.sqrt(kf1.R[0, 0])
+    y = i + np.random.randn()*np.sqrt(kf1.R[1, 1])
+    z = np.array([x, y])
 
-  # perform predict/update cycle
-  pred_states, pred_covars = my_imm.predict(
-      states=states, covars=covars, dt=1)
-  post_states, post_covars = my_imm.update(
-    measurement=z,
-    predicted_states=pred_states,
-    predicted_covars=pred_covars,
-  )
-  imm.predict()
-  imm.update(z)
-  print(imm.x.T)
+    # perform predict/update cycle for each filter
+    pred_states, pred_covars = my_imm.predict(
+        states=states, covars=covars, dt=1)
+    states, covars, x, P = my_imm.update(
+      measurement=z,
+      predicted_states=pred_states,
+      predicted_covars=pred_covars,
+    )
+    imm.predict()
+    imm.update(z)
+    
+    assert np.allclose(my_imm.mode_probs, imm.mu)
+    assert np.allclose(x, imm.x.squeeze())
+    assert np.allclose(P, imm.P.squeeze())
+
+if __name__ == '__main__':
+  pytest.main()
